2b9d1ebd9e580572161e5d2b7e1fed3c96d21f43
unknown <njlee326@gmail.com>
2023-06-03 15:40:09 -0700

    updates

--- 2.1 Vectors/sketch.js
+++ 2.1 Vectors/sketch.js
@@ -1,8 +1,24 @@
-function setup() {
-  createCanvas(400, 200);
-  background(100);
-}
+var a = function(r) {
+  let x;
+  let y;
+  let xspeed = 5;
+  let yspeed = 5;
+  r.setup = function() {
+    r.createCanvas(600, 500);
+    r.background(100);
+    x = r.width/2;
+    y = r.height/2;
+  };
+  r.draw = function() {
+    r.background(100);
+    x += xspeed;
+    y += yspeed;
+    r.ellipse(x, y, 50, 50);
+    if (x < 0) xspeed = 5;
+    if (x > r.width) xspeed = -5;
+    if (y < 0) yspeed = 5;
+    if (y > r.height) yspeed = -5;
+  };
+};
 
-function draw() {
-
-}
+var sketch1 = new p5(a, 'c1');
